1659d6412b94b1281e04fab8795ccadb50a396cb
Lee Bates <batespcm1981@gmail.com>
2023-12-24 18:21:38 +0000

    clean up the git cache

--- src/main/java/com/bankofjava/javabank/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bankofjava.javabank.repository;
-
-import com.bankofjava.javabank.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    Boolean existsByEmail(String email);
-}
--- src/main/java/com/bankofjava/javabank/service/impl/CustomerDetailsService.java
+++
@@ -1,9 +0,0 @@
-package com.bankofjava.javabank.service.impl;
-
-import com.bankofjava.javabank.dto.CustomerRequest;
-import com.bankofjava.javabank.dto.CustomerResponse;
-
-public interface CustomerDetailsService {
-
-   CustomerResponse createAccount(CustomerRequest customerRequest);
-}
