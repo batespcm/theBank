f26c95e2e15676f96c70a4fcf3b3ebee1c903dc5
Lee <batespcm1981@gmail.com>
2024-01-02 19:16:30 +0000

    HTTP codes added to controller and description added to swagger

--- src/main/java/com/bankofjava/javabank/JavaBankApplication.java
+++ src/main/java/com/bankofjava/javabank/JavaBankApplication.java
@@ -1,12 +1,26 @@
 package com.bankofjava.javabank;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Lee's Java Bank",
+                description = "A Little Java Bank back end to practice Java RestAPI development",
+                version = "1.0"
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Lee's Java Bank External Documentation",
+                url = "https://github.com/batespcm/theBank"
+        )
+)
 public class JavaBankApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaBankApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBankApplication.class, args);
+    }
 }
